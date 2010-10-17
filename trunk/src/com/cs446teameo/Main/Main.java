7b78ea822828bc14cc80563ac4382f504728b84d
jamzhao <jamzhao@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-17 05:27:04 +0000

    update to make class main the entry class for the application.

--- trunk/src/com/cs446teameo/Main/Main.java
+++ trunk/src/com/cs446teameo/Main/Main.java
@@ -1,6 +1,15 @@
 package com.cs446teameo.Main;
 
-public class Main {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
 	public static void main(String[] args){
 		return;
 	}
