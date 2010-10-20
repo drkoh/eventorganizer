088e9080c8d4e585c7883b55a378da96d8551eb6
calvinhung0104 <calvinhung0104@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-20 18:33:11 +0000

    added new Location type class and EventBuilder to build events

---
+++ src/com/cs446teameo/Event/EventBuilder.java
@@ -0,0 +1,31 @@
+package com.cs446teameo.Event;
+
+public class EventBuilder {
+	protected Event event;
+	
+	public Event getEvent(){
+		return event;
+	}
+	
+	public void createEvent(){
+		event = new Event();
+	}
+	
+	public void setDescription(String desc){
+		event.setDescription(new SimpleText(desc));
+	}
+	
+	public void setStatus(boolean s){
+		event.setStatus(new StatusSetting(s));
+	}
+	
+	public void setLocation(String l){
+		event.setLocation(new Location(l));
+	}
+	
+	public void setTime(int sYear, int sMonth, int sDay, int sHour, int sMin,
+						int eYear, int eMonth, int eDay, int eHour, int eMin){
+		event.setTime(new Segment(sYear, sMonth, sDay, sHour, sMin, eYear, eMonth, eDay, eHour, eMin));
+	}
+	
+}
