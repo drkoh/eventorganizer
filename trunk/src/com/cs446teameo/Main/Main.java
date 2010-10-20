1f41c2185233992d3ce7598fd8e9e476cf671aed
jamzhao <jamzhao@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-20 20:42:11 +0000

    Added the backend service, and try to ignore bin and gen

--- trunk/src/com/cs446teameo/Main/Main.java
+++ trunk/src/com/cs446teameo/Main/Main.java
@@ -1,21 +1,17 @@
 package com.cs446teameo.Main;
 
 import android.app.Activity;
-import android.content.Context;
+//import android.content.Context;
 import android.os.Bundle;
-import android.media.AudioManager;
+//import com.cs446teameo.Backend.*;
 
 public class Main extends Activity {
-	private AudioManager manager;
-	private Context context;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = getApplicationContext();
-        manager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        manager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
         setContentView(R.layout.main);
+        //getApplicationContext().startService(getIntent(BG));
     }
 
 	public static void main(String[] args){
