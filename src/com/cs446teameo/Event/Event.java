b4cbdecd5497652e5f87cf2e8218112ed51c680b
tanshuo89723149 <tanshuo89723149@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-20 16:28:18 +0000

    add in~!!!!!

--- src/com/cs446teameo/Event/Event.java
+++ src/com/cs446teameo/Event/Event.java
@@ -3,6 +3,7 @@ package com.cs446teameo.Event;
 import java.io.Serializable;
 
 import com.cs446teameo.Actor.BgProcesser;
+import com.cs446teameo.Parameter.ErrorCode;
 
 public class Event implements Activicable,Serializable{
 	/**
@@ -12,13 +13,15 @@ public class Event implements Activicable,Serializable{
 
 	TextField text_field;
 	Reminder reminder;
-	RepeatMode rpmode;
+	//RepeatMode rpmode;
 	StatusSetting st_setting;
 	TimeToAttack time_to_attack;
-	
+	Segment segment;
 	//event id
 	int eid;
-
+	
+	public Event(){	
+	}
 
 
 	public int trigger(BgProcesser bgprocesser) {
@@ -26,6 +29,15 @@ public class Event implements Activicable,Serializable{
 		return bgprocesser.trigger(this);
 	}
 	
+	public Event setTextField(String content){
+		
+		return this;
+	}
+	
+	//and the Integer returned is just the error code
+	public int setAttribute(EField something){
+		return ErrorCode.SUCCESS;
+	}
 	
 	//Note:
 	//should we implement Serializable in this class???
