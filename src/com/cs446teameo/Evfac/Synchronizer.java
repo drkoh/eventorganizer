c327ff3834fd3a48607e0df30b16dd5c72b5d288
calvinhung0104 <calvinhung0104@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-20 16:34:43 +0000

    added code to bring in events from Android Calendar

--- src/com/cs446teameo/Evfac/Synchronizer.java
+++ src/com/cs446teameo/Evfac/Synchronizer.java
@@ -1,5 +1,70 @@
-package com.cs446teameo.Evfac;
+//reference:
+//http://svn.jimblackler.net/jimblackler/trunk/workspace/AndroidReadCalendarExample/src/net/jimblackler/readcalendar/Example.java
+
+package Evfac;
+
+import java.util.Date;
+import java.util.HashSet;
+
+import Event.Event;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.text.format.DateUtils;
+
 
 public class Synchronizer extends EventCreator{
 
+	
+	public Synchronizer(){
+		
+	}
+	
+	public void syncCalendar(){
+		getCalendar();
+		
+	}
+	
+	private void getCalendar(){
+		Context context = null;
+		ContentResolver contentResolver = context.getContentResolver();
+		
+		String[] list = new String[]{"id", "name"};
+		Uri calendars = Uri.parse("content://calendar/calendars");
+		
+		Cursor c = contentResolver.query(calendars, list, null, null, null);
+		
+		HashSet<String> calendarIds = new HashSet<String>();
+		
+		while (c.moveToNext()){
+			String id = c.getString(0);
+			calendarIds.add(id);
+		}
+		
+		for (String id : calendarIds){
+			Uri.Builder builder = Uri.parse("content://calendar/instances/when").buildUpon();
+			long current = new Date().getTime();
+			ContentUris.appendId(builder, current - DateUtils.YEAR_IN_MILLIS);
+			ContentUris.appendId(builder, current + DateUtils.YEAR_IN_MILLIS);
+			
+			Cursor eventCursor = contentResolver.query(builder.build(),
+							new String[] {"title", "start", "end", "allDay"}, "Calendars.id=" + id,
+							null, "startDay ASC, startMinute ASC");
+			
+			while (eventCursor.moveToNext()){
+				String title = eventCursor.getString(0);
+				Date start = new Date(eventCursor.getLong(1));
+				Date end = new Date(eventCursor.getLong(2));
+				Boolean allDay = !eventCursor.getString(3).equals("0");
+				
+				Event e = new Event()
+			}
+			
+			
+		}
+	}
+	
+	
 }
