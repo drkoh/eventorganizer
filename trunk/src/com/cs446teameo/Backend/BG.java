4960a8cc38e0e7f03e75160950477d945cb1d9d0
taufiqbd@gmail.com <taufiqbd@gmail.com@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-10-26 18:13:07 +0000

    So I added some sorta notification on top of the Android Window (left to the vibrate/silent signals). These notifications will appear whenever an event starts/ends. I only made it say: vibrate and then default. Please update.

--- trunk/src/com/cs446teameo/Backend/BG.java
+++ trunk/src/com/cs446teameo/Backend/BG.java
@@ -1,11 +1,17 @@
 package com.cs446teameo.Backend;
 
+import android.app.Notification;
+
+import com.cs446teameo.Main.R;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.provider.SyncStateContract.Constants;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -23,10 +29,10 @@ public class BG extends Service {
 	
 	@Override
 	public void onCreate() {
-		TimerTask vibaret = new StatusChange();
+		TimerTask vibrate = new StatusChange();
 		long delay = 60000 - System.currentTimeMillis() % 60000;
 		Timer timer = new Timer();
-		timer.schedule(vibaret,delay, 60000);
+		timer.schedule(vibrate, delay, 60000);
 		Log.d("sd", "here wait 5sec");
 	}
 	@Override
@@ -45,39 +51,59 @@ public class BG extends Service {
 		return null;
 	}
 	
+	public void popup (String message, int duration)
+	{
+		Toast.makeText(Frame.owner, message, duration).show();
+	}
+	
 	class StatusChange extends TimerTask {
 		private AudioManager manager;
 		private Context context;
+		private CharSequence contentTitle = "";
+		private CharSequence contentText = "";
+
 		@Override
-		public void run(){
-			Log.d("error","run here");
-			Log.d("error","the current time" + System.currentTimeMillis()/1000);
+		public void run(){  
 			Event src = toTrigger();
 			boolean vibrate = false;
+			context = getApplicationContext();
+	        manager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
 			if(src == null){
 				if(currentEvent == null)
 					return;
-				Log.d("error","@3");
 				if(currentEvent.time.endTime()/60 == System.currentTimeMillis() / 60000){
-					Log.d("error","@4");
 					vibrate = false;
-				//	Toast toast = Toast.makeText(getApplicationContext(), "Event End\nEvent Description: "+currentEvent.description.toString() + " \nProfile: "+currentEvent.st_setting.status, Toast.LENGTH_LONG);
-				//	toast.show();
+
+					// Notification Starts
+					NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+					int icon = R.drawable.icon;
+					CharSequence text = "Event Ended, Profile: Default";
+					long when = System.currentTimeMillis();
+					Intent intent = new Intent(BG.this, StatusChange.class);
+					PendingIntent contentIntent = PendingIntent.getActivity(BG.this, 0, intent, 0);
+					Notification notification = new Notification(icon, text, when);
+					notification.setLatestEventInfo(BG.this, contentTitle, contentText, contentIntent);
+					notificationManager.notify(1, notification);
+					// Notification Ends
 				}
 			}else{
 				currentEvent = src;
-				Log.d("error","@0");
 				vibrate = src.st_setting.vibrate();
-				Log.d("error","@1");
-		//		Toast toast = Toast.makeText(getApplicationContext(), "Event Start\nEvent Description: "+src.description.toString() + "\nProfile: "+src.st_setting.status, Toast.LENGTH_LONG);
-		//		toast.show();
+
+				// Notification Starts
+				NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+				int icon = R.drawable.icon;
+				CharSequence text = "Event Started, Profile: Vibrate";
+				long when = System.currentTimeMillis();
+				Intent intent = new Intent(BG.this, StatusChange.class);
+				PendingIntent contentIntent = PendingIntent.getActivity(BG.this, 0, intent, 0);
+				Notification notification = new Notification(icon, text, when);
+				notification.setLatestEventInfo(BG.this, contentTitle, contentText, contentIntent);
+				notificationManager.notify(1, notification);
+				// Notification Ends
 			}
 			if(vibrate == currentStatus)
 				return;
-			Log.d("error","@5");
-			context = getApplicationContext();
-	        manager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-	        Log.d("error","@6");
 	        if(vibrate){
 	        	manager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 	        	currentStatus = true;
@@ -85,7 +111,6 @@ public class BG extends Service {
 	        	manager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 	        	currentStatus = false;
 	        }
-	        Log.d("error","@7");
 		}
 	}
 }
