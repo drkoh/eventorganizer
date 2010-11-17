c6432b772c134777331ab94b327076d075b10ef1
jamzhao <jamzhao@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-11-17 22:23:51 +0000

    database done

---
+++ src/com/cs446teameo/Storage/Database.java
@@ -0,0 +1,94 @@
+package com.cs446teameo.Storage;
+
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.*;
+import android.util.Log;
+
+public class Database {
+	private static final Database INSTANCE = new Database();
+	//Here we have to design our database considering concurrency
+	public static final String KEY_TITLE = "title";
+	public static final String KEY_BODY = "body";
+	public static final String KEY_ROWID = "_id"; 
+	private static final String TAG = "EventDatabase";
+	
+	private static DatabaseHelper mDbHelper;
+	private static SQLiteDatabase mDb;
+	
+	private static Context mCtx;
+	    
+	private static final String DATABASE_NAME = "EO";
+	private static final int DATABASE_VERSION = 2;
+    private static final String Event_TABLE_NAME = "Events";
+    private static final String Profile_TABLE_NAME = "Profile";
+    private static final String Event_TABLE_CREATE =
+    	"create table events (_id integer primary key autoincrement, "
+        + "name text not null, start_time integer not null, end_time integer not null);";
+    private static final String Profile_TABLE_CREATE =
+    	"create table profile (_id integer primary key autoincrement, "
+    	+ "name text not null, volume integer not null, vibrate boolean not null);";
+    
+    /**
+     * Constructor - takes the context to allow the database to be
+     * opened/created
+     * 
+     * @param ctx the Context within which to work
+     */
+    private Database() {
+    }
+    
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(Event_TABLE_CREATE);
+            db.execSQL(Profile_TABLE_CREATE);
+            Log.i("testdb", "here creat");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS table");
+            onCreate(db);
+        }
+    }
+
+    /**
+     * Open the notes database. If it cannot be opened, try to create a new
+     * instance of the database. If it cannot be created, throw an exception to
+     * signal the failure
+     * 
+     * @return this (self reference, allowing this to be chained in an
+     *         initialization call)
+     * @throws SQLException if the database could be neither opened or created
+     */
+    public static EventDatabase open(Context ctx) throws SQLException {
+    	if (mCtx == null) {
+    		mCtx = ctx;
+    		mDbHelper = new DatabaseHelper(mCtx);
+    		mDb = mDbHelper.getWritableDatabase();
+    	}
+        return INSTANCE;
+    }
+
+    public static void close() {
+        mDbHelper.close();
+    }
+    
+    public static Cursor query(String q) throws SQLException {
+        Cursor mCursor = mDb.rawQuery(q, new String[] {});
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+}
