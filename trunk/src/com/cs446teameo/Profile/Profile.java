3d9c3ff4901433e1b3256c9be070fa2e04d62f96
taufiqbd@gmail.com <taufiqbd@gmail.com@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-11-20 19:30:28 +0000

    Just some small edits that doesn't affect the functionalities much... Added basic colors to the manifest etc.

--- trunk/src/com/cs446teameo/Profile/Profile.java
+++ trunk/src/com/cs446teameo/Profile/Profile.java
@@ -3,55 +3,48 @@ package com.cs446teameo.Profile;
 import java.io.Serializable;
 
 public class Profile implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2789089601153968331L;
-	
-	public static final short SHUTDOWN = 0;
-	
-	private int pId;
-	private String description;
-	private boolean vibrate;
-	private int volume;
-	//short serial;
-	
-	//to add in more features here
-	/*public Profile(String description,int serial){
-		
-	}*/
-	
-	public Profile(String desc, boolean vib, int vol){
-		this.description = desc;
-		this.vibrate = vib;
-		this.volume = vol;
-	}
-	
-	public int getId(){
-		return pId;
-	}
-	
-	public void setDescription(String desc){
-		description = desc;
-	}
-	
-	public String getDescription(){
-		return description;
-	}
-	
-	public void setVibrate(boolean vib){
-		vibrate = vib;
-	}
-	
-	public boolean getVibrate(){
-		return vibrate;
-	}
-	
-	public void setVolume(int vol){
-		volume = vol;
-	}
-	
-	public int getVolume(){
-		return volume;
-	}
+
+        private static final long serialVersionUID = 2789089601153968331L;
+       
+        public static final short SHUTDOWN = 0;
+       
+        private int pId;
+        private String description;
+        private boolean vibrate;
+        private int volume;
+       
+        public Profile(String desc, boolean vib, int vol)
+        {
+			this.description = desc;
+			this.vibrate = vib;
+			this.volume = vol;
+        }
+       
+        public int getId(){
+                return pId;
+        }
+       
+        public void setDescription(String desc){
+                description = desc;
+        }
+       
+        public String getDescription(){
+                return description;
+        }
+       
+        public void setVibrate(boolean vib){
+                vibrate = vib;
+        }
+       
+        public boolean getVibrate(){
+                return vibrate;
+        }
+       
+        public void setVolume(int vol){
+                volume = vol;
+        }
+       
+        public int getVolume(){
+                return volume;
+        }
 }
