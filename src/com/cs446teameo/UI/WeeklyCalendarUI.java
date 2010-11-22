fc8aa88b510923baed21e2e9d60c92749475c1f7
taufiqbd <taufiqbd@gmail.com>
2010-11-22 07:26:45 +0000

    So this edition dynamically updates all the text for the UIs. Now you can switch the day, month, year etc. in all the Calendar Views. All texts are calculated automatically (always starting from current day, month, or year).

--- src/com/cs446teameo/UI/WeeklyCalendarUI.java
+++ src/com/cs446teameo/UI/WeeklyCalendarUI.java
@@ -1,5 +1,8 @@
 package com.cs446teameo.UI;
 
+import java.util.Calendar;
+
+import android.graphics.Typeface;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,7 +29,22 @@ public class WeeklyCalendarUI extends Frame{
 	TableLayout thursdayLayout = null;
 	TableLayout fridayLayout = null;
 	TableLayout saturdayLayout = null;
-	
+	TextView sundayText = null;
+	TextView mondayText = null;
+	TextView tuesdayText = null;
+	TextView wednesdayText = null;
+	TextView thursdayText = null;
+	TextView fridayText = null;
+	TextView saturdayText = null;
+	int currentYear, currentMonth, currentDay, currentDayOfTheWeek;
+	Calendar calendar;
+	String dateString;
+	String[] monthNames = {"January", "February", "March", 
+			"April", "May", "June", "July", "August", 
+			"September", "October", "November", "December"};
+	String[] dayNames = new String[] { "Sunday", "Monday", "Tuesday", 
+			"Wednesday", "Thusday", "Friday", "Saturday" };
+
 	private static WeeklyCalendarUI _instance = null;
 	
 	private WeeklyCalendarUI() {
@@ -46,6 +64,24 @@ public class WeeklyCalendarUI extends Frame{
 	public void registeListener() {
 		// TODO Auto-generated method stub
 		
+		leftButton.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View arg0) {
+				calendar.add(Calendar.DAY_OF_MONTH, -7);
+				setUI(calendar);
+			}
+		});
+		
+
+		rightButton.setOnClickListener(new OnClickListener(){
+			
+			@Override
+			public void onClick(View arg0) {
+				calendar.add(Calendar.DAY_OF_MONTH, 7);
+				setUI(calendar);
+			}
+		});
 		
 		// Create Button
 		yearlyTab.setOnClickListener(new OnClickListener(){
@@ -134,6 +170,113 @@ public class WeeklyCalendarUI extends Frame{
 		this.thursdayLayout = (TableLayout) owner.findViewById(R.weeklycalendar.thursdayLayout);
 		this.fridayLayout = (TableLayout) owner.findViewById(R.weeklycalendar.fridayLayout);
 		this.saturdayLayout = (TableLayout) owner.findViewById(R.weeklycalendar.saturdayLayout);
+		this.sundayText = (TextView) owner.findViewById(R.weeklycalendar.sundayText);
+		this.mondayText = (TextView) owner.findViewById(R.weeklycalendar.mondayText);
+		this.tuesdayText = (TextView) owner.findViewById(R.weeklycalendar.tuesdayText);
+		this.wednesdayText = (TextView) owner.findViewById(R.weeklycalendar.wednesdayText);
+		this.thursdayText = (TextView) owner.findViewById(R.weeklycalendar.thursdayText);
+		this.fridayText = (TextView) owner.findViewById(R.weeklycalendar.fridayText);
+		this.saturdayText = (TextView) owner.findViewById(R.weeklycalendar.saturdayText);
+		calendar = Calendar.getInstance();
+		setUI(calendar);
+	}
+	
+	// Sets all the text-based UI components
+	public void setUI (Calendar c)
+	{
+		Calendar tempCalendar;
+		String beginTitle, endTitle;
+		
+		// Calculate current day
+        currentYear = c.get(Calendar.YEAR);
+        currentMonth = c.get(Calendar.MONTH);
+        currentDay = c.get(Calendar.DAY_OF_MONTH);
+        currentDayOfTheWeek = c.get(Calendar.DAY_OF_WEEK);
+        dateString = dayNames[currentDayOfTheWeek - 1] + ", " + monthNames[currentMonth]  + " " + currentDay + ", " + currentYear;
+        
+        // Set the date's text for Sunday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.SUNDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        beginTitle = tempCalendar.get(Calendar.DAY_OF_MONTH) + "/" +
+		tempCalendar.get(Calendar.MONTH) + "/" +
+		tempCalendar.get(Calendar.YEAR);
+        sundayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Monday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        mondayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Tuesday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.TUESDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        tuesdayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Wednesday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.WEDNESDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        wednesdayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Thursday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.THURSDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        thursdayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Friday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.FRIDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        fridayText.setText(dateString);
+        setEventsUI(tempCalendar);
+        
+        // Set the date's text for Saturday
+        tempCalendar = (Calendar)c.clone();
+        tempCalendar.set(Calendar.DAY_OF_WEEK, Calendar.SATURDAY);
+        dateString = dayNames[tempCalendar.get(Calendar.DAY_OF_WEEK) - 1] + ", " + 
+        			monthNames[tempCalendar.get(Calendar.MONTH)]  + " " + 
+        			tempCalendar.get(Calendar.DAY_OF_MONTH) + ", " + 
+        			tempCalendar.get(Calendar.YEAR);
+        endTitle = tempCalendar.get(Calendar.DAY_OF_MONTH) + "/" +
+		tempCalendar.get(Calendar.MONTH) + "/" +
+		tempCalendar.get(Calendar.YEAR);
+        saturdayText.setText(dateString); 
+        setEventsUI(tempCalendar);   
+		
+        // Set the week date's text
+        date.setText(beginTitle + " - " + endTitle);
+	}
+	
+	// Sets al the event-based UI components
+	public void setEventsUI(Calendar c)
+	{
 	}
 
 }
