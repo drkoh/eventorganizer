0ba94fdee060181bba9673140df7e063472cbc4c
taufiqbd@gmail.com <taufiqbd@gmail.com@2357a7c7-553a-66d4-bd01-9aa6abcdc188>
2010-11-30 16:59:18 +0000

    Helps update the profile's in EditProfile. Now you the UI retrieves the profiles's info and updates the widgets accordingly. Also, profile id is working (I was having trouble - forgetting to use the constructor with pID when I was listing all profiles).

--- trunk/src/com/cs446teameo/UI/EditProfileUI.java
+++ trunk/src/com/cs446teameo/UI/EditProfileUI.java
@@ -24,7 +24,7 @@ public class EditProfileUI extends Frame{
 	Button exitButton = null;
 	Button editButton = null;
 	Button deleteButton = null;
-	
+	ArrayList<Profile>list = new ArrayList<Profile>();
 	
 	//Here You should first load on all created profiles, or how can you delete it
 	
@@ -50,13 +50,26 @@ public class EditProfileUI extends Frame{
 	public void registeListener() {
 		// TODO Auto-generated method stub
 		
+		name.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+		    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) 
+		    {
+		    	String spinnerSelection = (String)name.getItemAtPosition(pos);
+				volume.setProgress(getProfileVolume(spinnerSelection));
+				volumeValue.setText(" " + getProfileVolume(spinnerSelection));
+				vibrate.setChecked(getProfileVibrate(spinnerSelection));
+		    }
+		    public void onNothingSelected(AdapterView<?> parent) { }
+		});
+		
 		// Create Button
 		deleteButton.setOnClickListener(new OnClickListener(){
 			
 			@Override
 			public void onClick(View arg0) {
 				// TODO!!!
-				int res = ProfileManager.getInstance().deleteProfile(((Profile)name.getSelectedItem()).getId());
+				int spinnerPosition = name.getSelectedItemPosition();
+				String spinnerSelection = (String)name.getItemAtPosition(spinnerPosition);
+				int res = ProfileManager.getInstance().deleteProfile(getProfileID(spinnerSelection));
 				if (res != ErrorCode.SUCCESS){
 					Log.e(field, "Delete Profile Error");
 					return;
@@ -106,7 +119,16 @@ public class EditProfileUI extends Frame{
 			@Override
 			public void onClick(View arg0) {
 				// TODO!!!
-				Log.i(field,"trigger edit button");
+				int spinnerPosition = name.getSelectedItemPosition();
+				String spinnerSelection = (String)name.getItemAtPosition(spinnerPosition);
+				Profile profile = new Profile(spinnerSelection, vibrate.isChecked(), volume.getProgress());
+				int res = ProfileManager.getInstance().updateProfile(profile, getProfileID(spinnerSelection));
+				if(res != ErrorCode.SUCCESS){
+					//TODO: ADD AN NOTIFICATION TO THE USER
+					Log.e(field,"Add Event Error: " + res);
+					return;
+				}
+				Log.i(field,"trigger create button");
 				ProfileUI.contextSwitch();
 			}
 		});
@@ -138,15 +160,55 @@ public class EditProfileUI extends Frame{
 	
 	private void loadProfile()
 	{
-		ArrayList<Profile>list = new ArrayList<Profile>();
+		list.clear();
 		ProfileManager.getInstance().listProfile(list);
 		String profileNames[] = new String[list.size()];
+		Log.d(field, "Load Profiles: ");
 		for(int i = 0; i < list.size(); i++)
 		{
 			profileNames[i] = (list.get(i)).getDescription();
+			Log.d(field, "- Profile Name: " + (list.get(i)).getDescription());
+			Log.d(field, "- Profile ID: " + (list.get(i)).getId());
 		}
 		ArrayAdapter<CharSequence> profileadapter = new ArrayAdapter<CharSequence>(owner, android.R.layout.simple_spinner_item, profileNames);
+		name.setAdapter(null);
 		name.setAdapter(profileadapter);
+	}
+	
+	private int getProfileID(String profileName)
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(profileName.equals((list.get(i)).getDescription()))
+			{
+				return (list.get(i)).getId();
+			}
+		}
+		return -1;
+	}
+	
+	private int getProfileVolume(String profileName)
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(profileName.equals((list.get(i)).getDescription()))
+			{
+				return (list.get(i)).getVolume();
+			}
+		}
+		return -1;
+	}
+		
+	private boolean getProfileVibrate(String profileName)
+	{
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(profileName.equals((list.get(i)).getDescription()))
+			{
+				return (list.get(i)).getVibrate();
+			}
+		}
+		return false;
 	}
 }
 
